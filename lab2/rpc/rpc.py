7081692d8b7dc314bad0322830d506a50ab3a336
Jakob Kohler <schasch.dev@protonmail.com>
2024-11-07 20:38:27 +0100

    Initial implementation lab2

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,3 +1,4 @@
+import time
 import constRPC
 
 from context import lab_channel
@@ -25,12 +26,19 @@ class Client:
     def stop(self):
         self.chan.leave('client')
 
-    def append(self, data, db_list):
+    def append(self, data, db_list, cb):
         assert isinstance(db_list, DBList)
-        msglst = (constRPC.APPEND, data, db_list)  # message payload
+        msglst = (constRPC.APPEND, data, db_list) 
         self.chan.send_to(self.server, msglst)  # send msg to server
-        msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
+        msgrcv = self.chan.receive_from(self.server) 
+        if(msgrcv[1] == constRPC.OK):
+            print('Recieved ACK')
+        else:
+            print(msgrcv)
+            
+        res = self.chan.receive_from(self.server)
+        cb(self, res[1])
+        # return msgrcv[1]  # pass it to caller
 
 
 class Server:
@@ -51,7 +59,9 @@ class Server:
             if msgreq is not None:
                 client = msgreq[0]  # see who is the caller
                 msgrpc = msgreq[1]  # fetch call & parameters
-                if constRPC.APPEND == msgrpc[0]:  # check what is being requested
+                if constRPC.APPEND == msgrpc[0]:
+                    self.chan.send_to({client}, constRPC.OK)
+                    time.sleep(10)
                     result = self.append(msgrpc[1], msgrpc[2])  # do local call
                     self.chan.send_to({client}, result)  # return response
                 else:
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,7 +1,12 @@
+import time
 import rpc
 import logging
+import threading
 
 from context import lab_logging
+
+def show_rpc_result(cl, result):
+    print("Result: {}".format(result.value))
 
 lab_logging.setup(stream_level=logging.INFO)
 
@@ -9,8 +14,17 @@ cl = rpc.Client()
 cl.run()
 
 base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
 
-print("Result: {}".format(result_list.value))
+append_thread = threading.Thread(target=cl.append, args=('bar', base_list, show_rpc_result))
+append_thread.start()
 
-cl.stop()
+
+for i in range(11):
+    print(f"Client still active [{i}]")
+    time.sleep(1)
+
+cl.close()
+
+# Unklar: Wann client schliessen?
+# Entweder im callback oder so wie aktuell implementiert (Wenn der Client vor dem return des callbacks geschlossen wird, stuerzt der Server ab)
+# Alternativ: Client laeuft im endless loop bis der callback zurueckkommt
